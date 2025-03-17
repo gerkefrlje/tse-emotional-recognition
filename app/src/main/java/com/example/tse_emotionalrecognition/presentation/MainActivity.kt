429aa54b0870f2e70995fefb1b83db3b341ac0dd
unmlm <unmlm@student.kit.edu>
2025-03-17 22:45:37 +0100

    removed unnecessary imports

--- app/src/main/java/com/example/tse_emotionalrecognition/presentation/MainActivity.kt
+++ app/src/main/java/com/example/tse_emotionalrecognition/presentation/MainActivity.kt
@@ -1,6 +1,5 @@
 package com.example.tse_emotionalrecognition.presentation
 
-import android.Manifest
 import android.content.Context
 import android.content.Intent
 import android.content.SharedPreferences
@@ -30,9 +29,6 @@ import androidx.work.NetworkType
 import androidx.work.PeriodicWorkRequest
 import androidx.work.WorkManager
 import com.example.tse_emotionalrecognition.common.data.database.UserDataStore
-import com.example.tse_emotionalrecognition.common.data.database.UserRepository
-import com.example.tse_emotionalrecognition.common.data.database.entities.AffectData
-import com.example.tse_emotionalrecognition.common.data.database.entities.AffectType
 import com.example.tse_emotionalrecognition.common.data.database.entities.InterventionStats
 import com.example.tse_emotionalrecognition.common.data.database.entities.TAG
 import com.example.tse_emotionalrecognition.common.data.database.utils.CommunicationDataSender
@@ -43,12 +39,10 @@ import com.example.tse_emotionalrecognition.presentation.utils.EmojiState
 import com.example.tse_emotionalrecognition.presentation.utils.scheduleDailyEmojiUpdateWorkManager
 import com.example.tse_emotionalrecognition.presentation.utils.updateEmoji
 import com.example.tse_emotionalrecognition.presentation.utils.DataCollectReciever
-import com.example.tse_emotionalrecognition.presentation.utils.DataCollectService
 import com.example.tse_emotionalrecognition.presentation.utils.DataCollectWorker
 import java.util.concurrent.TimeUnit
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.launch
 import kotlinx.serialization.encodeToString
 import kotlinx.serialization.json.Json
@@ -62,7 +56,7 @@ class MainActivity : ComponentActivity() {
 
     companion object {
         val DEBUG_TAG = "MainActivity"
-        val trackerID = 1L
+        const val trackerID = 1L
 
     }
 
@@ -195,6 +189,18 @@ fun SelectIntervention(userRepository: com.example.tse_emotionalrecognition.comm
             }
             item {
                 Button(
+                    onClick = {  val newEmojiState = EmojiState.entries.random()
+                        updateEmoji(context, newEmojiState)
+
+                        // Force UI Update
+                        currentEmojiState = newEmojiState },
+                    modifier = Modifier.fillMaxWidth()
+                ) {
+                    Text("Randomize Emoji")
+                }
+            }
+            item {
+                Button(
                     onClick = {
                         val intent = Intent(context, InterventionOverviewActivity::class.java)
                         context.startActivity(intent)
@@ -245,16 +251,20 @@ fun SelectIntervention(userRepository: com.example.tse_emotionalrecognition.comm
             }
             item {
                 Button(
-                    onClick = {  val newEmojiState = EmojiState.entries.random()
-                        updateEmoji(context, newEmojiState)
+                    onClick = {
 
-                        // Force UI Update
-                        currentEmojiState = newEmojiState },
+                        val intent = Intent(context, SendDataActivity::class.java)
+                        context.startActivity(intent)
+                    },
                     modifier = Modifier.fillMaxWidth()
                 ) {
-                    Text("Randomize Emoji")
+                    Text("Transfer Test")
                 }
             }
+            item {
+                Spacer(modifier = Modifier.height(16.dp))
+            }
+
         }
     }
 }
